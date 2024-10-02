0d844ded6e68da7fb940df5085466514584684e6
Tirth chokshi <chokshitirth4@gmail.com>
2024-10-02 13:03:22 +0530

    prod fix1

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
+let prisma
+
 if (process.env.NODE_ENV === 'production') {
-  let prisma = new PrismaClient()
+  prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient()
